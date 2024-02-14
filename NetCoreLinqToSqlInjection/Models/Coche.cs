2adeb99c60ce8d00ab2970de58e70422f2476c48
RazvanLucacii <rlucaci48@gmail.com>
2024-02-14 12:35:07 +0100

    Add project files.

---
+++ NetCoreLinqToSqlInjection/Models/Coche.cs
@@ -0,0 +1,40 @@
+﻿namespace NetCoreLinqToSqlInjection.Models
+{
+    public class Coche : ICoche
+    {
+        public string Marca {  get; set; }
+        public string Modelo { get; set; }
+        public string Imagen {  get; set; }
+        public int Velocidad { get; set; }
+        public int VelocidadMaxima { get; set; }
+
+        public Coche ()
+        {
+            this.Marca = "Seat";
+            this.Modelo = "Leon";
+            this.Imagen = "seat.jpg";
+            this.Velocidad = 0;
+            this.VelocidadMaxima = 250;
+        }
+
+        public int Acelerar()
+        {
+            Velocidad += 20;
+            if (Velocidad > VelocidadMaxima)
+            {
+                Velocidad = VelocidadMaxima;
+            }
+            return Velocidad;
+        }
+
+        public int Frenar()
+        {
+            Velocidad -= 20;
+            if (Velocidad < 0)
+            {
+                Velocidad = 0;
+            }
+            return Velocidad;
+        }
+    }
+}
---
+++ NetCoreLinqToSqlInjection/Models/Deportivo.cs
@@ -0,0 +1,40 @@
+﻿namespace NetCoreLinqToSqlInjection.Models
+{
+    public class Deportivo: ICoche
+    {
+        public Deportivo() 
+        {
+            this.Marca = "Audi";
+            this.Modelo = "R8";
+            this.Imagen = "r8.jpg";
+            this.Velocidad = 0;
+            this.VelocidadMaxima = 330;
+        }
+
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public string Imagen { get; set; }
+        public int Velocidad { get; set; }
+        public int VelocidadMaxima { get; set; }
+
+        public int Acelerar()
+        {
+            this.Velocidad += 60;
+            if(this.Velocidad >= this.VelocidadMaxima)
+            {
+                this.Velocidad = this.VelocidadMaxima;
+            }
+            return this.Velocidad;
+        }
+
+        public int Frenar()
+        {
+            Velocidad -= 30;
+            if (Velocidad < 0)
+            {
+                Velocidad = 0;
+            }
+            return Velocidad;
+        }
+    }
+}
